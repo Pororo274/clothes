baabac5a2326648325d702e3330aa1cafe721dda
Pororo <72220877+Pororo274@users.noreply.github.com>
2024-01-24 09:02:39 +0000

    add hero block

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header";
+import Banner from "./components/banner";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
+      <Banner />
     </>
   );
 }
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,11 +1,13 @@
-import banner from "../assets/img/banner.jpg";
-
 export default function Banner() {
   return (
     <section className="hero">
       <div className="container">
         <div className="hero-banner">
-          <img src={banner} alt="" className="hero-banner__i" />
+          <h1 className="h1">Онлайн магазин</h1>
+          <p className="hero__p">* лучший по продажам среди конкурентов</p>
+          <a href="" className="hero-button">
+            Каталог
+          </a>
         </div>
       </div>
     </section>
