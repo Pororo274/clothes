9a8f5fce336cb232bbbb0efd999060dde940b8b2
Pororo <72220877+Pororo274@users.noreply.github.com>
2024-01-24 08:34:52 +0000

    add hero banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,7 +1,11 @@
+import banner from "../assets/img/banner.jpg";
+
 export default function Banner() {
-    return (
-        <section className="hero">
-            
-        </section>
-    )
+  return (
+    <section className="hero">
+      <div className="hero-banner">
+        <img src={banner} alt="" className="hero-banner__i" />
+      </div>
+    </section>
+  );
 }
